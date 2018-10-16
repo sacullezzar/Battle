21907bb3dc23925bfa23fb70ffe329de3f622641
sacullezzar <lucas.razzell@holidayextras.com>
2018-10-16 15:45:15 +0100

    implemented DRY testing

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -7,20 +7,14 @@
 
 feature 'Filling in player names' do
   scenario 'players are asked for their names which appear on the page' do
-    visit('/')
-    fill_in :player_1_name, with: 'Lucas'
-    fill_in :player_2_name, with: 'Zoe'
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Lucas vs Zoe')
   end
 end
 
 feature 'View Hitpoints' do
   scenario "Players can see each other's hitpoints" do
-    visit ('/')
-    fill_in :player_1_name, with: 'Lucas'
-    fill_in :player_2_name, with: 'Zoe'
-    click_button('Submit')
-    expect(page).to have_content('Lucas HP = 100')
+    sign_in_and_play
+    expect(page).to have_content('Lucas HP = 100 Zoe HP = 100')
   end
 end
