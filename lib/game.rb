318a43ef3c84bbc5dbe5f86864dfa58d30b7535a
sacullezzar <lucas.razzell@holidayextras.com>
2018-10-17 16:15:15 +0100

    skinny controllers

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,30 @@
-require 'player'
+require './lib/player'
+
+# class Game
+#   def attack(player)
+#     player.receive_damage
+#   end
+# end
+
 
 class Game
+
+attr_reader :player_1, :player_2
+
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+  end
+
   def attack(player)
     player.receive_damage
   end
+
+  def player_1
+    @players.first
+  end
+
+  def player_2
+    @players.last
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,21 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(dummy1, dummy2) }
   let(:dummy1) {double :player }
   let(:dummy2) {double :player }
+
+  describe '#player_1' do
+    it "retrieves player 1" do
+      expect(game.player_1).to eq dummy1
+    end
+  end
+
+  describe '#player_2' do
+    it "retrieves player 2" do
+      expect(game.player_2).to eq(dummy2)
+    end
+  end
 
   describe "#attack" do
     it "reduces HP" do
